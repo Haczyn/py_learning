ad2f85eaf4616e97db05779bfc9f404c412b7f7d
haczyn <haczynek@gmail.com>
2022-02-23 23:26:38 +0100

    kalkulator version 1

--- kalkulator.py
+++ kalkulator.py
@@ -1,2 +1,29 @@
+﻿
+def dodawanie(x,y):
+    sum = int(x) + int(y)
+    return sum
+def odejmowanie(x,y):
+    sum = int(x) - int(y)
+    return sum
+def mnozenie(x,y):
+    sum = int(x) * int(y)
+    return sum
+def dzielenie(x,y):
+    sum = int(x) / int(y)
+    return sum
 
-test
+n = input('wprowadz liczbe: ')
+n1 = input('wprowadz druga liczbe:')
+znak = input('Wprowadz znak(+,-,*,/)')
+
+
+if znak == "+" :
+    print (dodawanie(n,n1))
+elif znak == "-" :
+    print (odejmowanie(n,n1))
+elif znak == "*" :
+    print (mnozenie(n,n1))
+elif znak == "/" :
+    print (dzielenie(n,n1))
+else:
+    print("Wprowadzono nieprawidłowy znak!") 
